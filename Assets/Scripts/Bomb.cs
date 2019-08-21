d9b1a57d6fc0d71bfb9fcc6d362081f71af58588
Alex Liu <gloria_domini@hotmail.com>
2019-08-21 16:01:16 +1000

    Bomb Fixed (Still Bugged)

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using Mirror;
 public class Bomb : NetworkBehaviour
 {
+    [SerializeField]
+    private string enemyTag;
     public float explosionRadius = 2f;
     public float explosionDelay = 2f;
     private void OnDrawGizmos()
@@ -27,11 +29,23 @@ public class Bomb : NetworkBehaviour
     [Command]
     void CmdExplode(Vector3 position, float radius)
     {
+
         Collider[] hits = Physics.OverlapSphere(transform.position, explosionRadius);
+        
         foreach (var hit in hits)
         {
-            NetworkServer.Destroy(hit.gameObject);
-            Destroy(gameObject);
+            Enemy enemy = hit.GetComponent<Enemy>();
+            if(enemy.tag == enemyTag)
+            {
+                NetworkServer.Destroy(hit.gameObject);
+                Destroy(gameObject);
+
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            
         }
     }
     
